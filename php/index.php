466fb09909db28189daafc0f73ca6909afb9ee18
illutek <stefanvandenborne@gmail.com>
2020-09-27 15:24:46 +0200

    Drupal theme to php site  Changes to be committed: 	deleted:    arborv2.info.yml 	deleted:    arborv2.libraries.yml 	deleted:    arborv2.theme 	modified:   css/styles.css 	modified:   dist/css/styles.css 	modified:   dist/css/styles.css.map 	new file:   dist/images/logo-full.png 	new file:   dist/images/logo_accent-clr.png 	new file:   dist/images/logo_accent-two-clr.png 	modified:   dist/js/all.js 	new file:   html/contact-overlay.html 	new file:   html/cookie-alert.html 	new file:   html/footer.html 	new file:   html/head.html 	new file:   html/header.html 	modified:   html/index.html 	new file:   html/menu-overlay.html 	new file:   images/logo-full.png 	new file:   images/logo_accent-clr.png 	new file:   images/logo_accent-two-clr.png 	new file:   js/contact.js 	new file:   js/menu.js 	new file:   php/copyright.php 	new file:   php/index.php 	modified:   sass/00-abstracts/_mixins.scss 	new file:   sass/03-regions/_contact-overlay.scss 	modified:   sass/03-regions/_index.scss 	new file:   sass/03-regions/_menu-overlay.scss 	new file:   sass/04-components/_buttons.scss 	new file:   sass/04-components/_close-btn.scss 	modified:   sass/04-components/_contact-btn.scss 	new file:   sass/04-components/_cookiealert.scss 	modified:   sass/04-components/_index.scss

---
+++ php/index.php
@@ -0,0 +1,51 @@
+<?php echo file_get_contents("../html/head.html"); ?>
+
+<body>
+    
+<?php echo file_get_contents("../html/menu-overlay.html"); ?>
+
+<?php echo file_get_contents("../html/contact-overlay.html"); ?>
+
+    <div class="r-parent-container">
+
+    <?php echo file_get_contents("../html/header.html"); ?>
+    
+
+    <div class="r-parent-main">
+        <main>
+            <div class="r-content">
+                <div class="container">
+                    <h2>Arbor Service <span>Boomverzorging tuinonderhoud</span></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium maxime, ab fugiat ad
+                        commodi cupiditate enim atque animi dolorum! Eaque ex quasi molestiae vitae tenetur, officia at
+                        odio corrupti quas!
+                        Maiores quisquam dolorum optio, ipsum dignissimos veniam temporibus cupiditate quaerat dolore
+                        perferendis blanditiis, rerum facilis natus aliquid beatae quas. Maiores pariatur vitae unde
+                        inventore molestiae nesciunt quaerat, beatae at eos.
+                        Quam voluptates repudiandae quidem obcaecati necessitatibus qui aliquid, reprehenderit quae
+                        molestiae labore? Reiciendis odio dolore impedit! Maxime pariatur quas ab, similique repellat
+                        sint culpa inventore, voluptatem iusto magnam nisi nihil!
+                        Odit facilis asperiores voluptatibus dolorem dolor cum atque fuga. Inventore perferendis libero
+                        illum? Enim eos ducimus nostrum placeat, ea magni nesciunt beatae neque, asperiores inventore
+                        distinctio aperiam. Doloremque, ullam suscipit.
+                        Sapiente eveniet nostrum doloremque aliquam, recusandae neque maxime officia dolorum
+                        reprehenderit quisquam, ea rerum tempora ipsum odio iusto alias consectetur! Earum architecto
+                        distinctio reiciendis ratione assumenda impedit rerum provident molestias.
+                    </p>
+                </div>
+            </div>
+        </main>
+
+        <?php echo file_get_contents("../html/footer.html"); ?>
+        
+    </div>
+
+    </div>
+ 
+    <?php echo file_get_contents("../html/cookie-alert.html"); ?>
+
+</body>
+
+<script src="../dist/js/all.js"></script>
+
+</html>
